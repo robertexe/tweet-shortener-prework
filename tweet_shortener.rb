3f22de2a252025303d7da752f42f6324997aba57
Robert Tabares <robtabares92@gmail.com>
2018-05-29 01:45:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,3 +42,11 @@ def selective_tweet_shortener(tweet)
     return tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    tweet.truncate(140)
+  else
+    return tweet
+  end
+end
