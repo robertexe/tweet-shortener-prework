2668988018308055ddd8661f10ff429ff8b24e0f
Robert Tabares <robtabares92@gmail.com>
2018-05-29 01:26:55 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,8 +36,8 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length >= 141
-    shortened = word_substituter(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
   end
   return tweet
 end
