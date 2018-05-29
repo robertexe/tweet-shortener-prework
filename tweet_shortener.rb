88928d6e4f6426bd338703370364576246b078fb
Robert Tabares <robtabares92@gmail.com>
2018-05-29 00:12:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  return dictionary
+end
+
+def word_substituter(str)
+  str_array = str.split(' ')
+  output_array = [];
+  for word in str_array do
+   if dictionary.key?(word)
+      word = dictionary[word]
+   end
+   output_array << word
+  end
+  output = output_array.join(' ')
+  return output
+end
